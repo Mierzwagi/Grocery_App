9e3d22ee10e7c1f6a2ab69988f8e96dd7bdd132a
Gioavnna Mierzwa <giihmierzwa111@outlook.com>
2024-05-08 20:39:50 -0300

    Criando .env

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 //Aplicação para utilizar bancos relacionais
+require("dotenv").config();
 const Sequelize = require('sequelize');
 
 //Criando instancia do sequilize
-const sequelize = new Sequelize('quitanda', 'root', 'root',{
+const sequelize = new Sequelize('quitanda', process.env.DB_PASSWORD, 'root',{
     //Dialetq = qual banco utilizado e onde está
     dialect: 'mysql',
 
@@ -10,5 +11,5 @@ const sequelize = new Sequelize('quitanda', 'root', 'root',{
 
 
 
-//Exportar o arquivo de configuraçã do banco
+//Exportar o arquivo de configuração do banco
 module.exports = sequelize
